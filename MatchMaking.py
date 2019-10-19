912a698b1326a596d75f4fc6a28cb825e6736c00
Pulkit <jainpulkit206@gmail.com>
2019-10-20 03:21:54 +0530

    Made a more generalised way with comments added at each step.

--- MatchMaking.py
+++ MatchMaking.py
@@ -1,15 +1,49 @@
-from itertools import combinations
+from itertools import combinations   					#Importing combinations module from the itertools package
 
-arr=[('bleh',85),('Aequitas',90),('Aks',87),('Iam',20)]
-m=2
-combi=combinations(arr,m)
-newarr=[]
-for i in combi:
-	avg=(i[0][1]+i[1][1])/m
-	newarr.append((i[0][0],i[1][0],avg))
+arr=[('bleh',85),('Aequitas',90),('Aks',87),('Iam',20)] #Players List
 
-validmatches = list(match for match in combinations(newarr, 2) if (match[0][0] not in match[1] and match[0][1] not in match[1]))
+m=2		#M vs M Match
 
-matches=sorted(validmatches,key=lambda x:abs(x[0][2]-x[1][2]))
+combi=combinations(arr,m) #All combinations of team with m players
+
+teams=[]  #Empty array list to store all the teams
+
+
+# Creating all the teams and calculating average score of each team
+
+for team in combi:
+	names=[]
+	avg=0
+	for j in range(m):
+		avg+=team[j][1]
+		names.append(team[j][0])
+	avg=avg/m
+	avg = float("{0:.2f}".format(avg))  #Restricting the average to 2 decimal place and type casing it back to float
+	names.append(avg)
+	teams.append(tuple(names))			#Appending each team at the end of each iteration to the teams array list
+
+
+validmatches=[]							#Empty array list to store all the valid m vs m matches
+
+
+#Deciding whether a match is valid or not from all the possible m vs m combinations
+
+for match in combinations(teams,2):
+	flag=True
+	for i in range(m):
+		if(match[0][i] not in match[1]):
+			continue
+		else:
+			flag=False
+			break
+	if(flag):
+		validmatches.append(match)
+
+
+#Sorting all the valid matches according to the quality of each match
+
+matches=sorted(validmatches,key=lambda x:abs(int(x[0][m])-int(x[1][m])))
+
+#Printing out all the valid matches
 for i in matches:
 	print(i)
