d50f21786aa2b4203812c9556450b13c0e78d091
Pulkit <jainpulkit206@gmail.com>
2019-10-20 02:47:44 +0530

    Made all possible combinations of 2vs2 matches sorted by quality

--- MatchMaking.py
+++ MatchMaking.py
@@ -1,10 +1,14 @@
 from itertools import combinations
 
-arr=[('bleh',85),('Aequitas',90),('Aks',87),('Iam',20),('bill',50)]
+arr=[('bleh',85),('Aequitas',90),('Aks',87),('Iam',20)]
 m=2
 combi=combinations(arr,m)
 newarr=[]
 for i in combi:
 	avg=(i[0][1]+i[1][1])/m
 	newarr.append((i[0][0],i[1][0],avg))
-print(newarr)
+
+finalteams=combinations(newarr,2)
+matches=sorted(finalteams,key=lambda x:abs(x[0][2]-x[1][2]))
+for i in matches:
+	print(i)
