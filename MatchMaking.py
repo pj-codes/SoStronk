de679e49e9f858fb99d93db96409377f5a3b9a36
Pulkit <jainpulkit206@gmail.com>
2019-10-20 02:44:33 +0530

    Made all possible combinations of team with 2 players with avg score

--- MatchMaking.py
+++ MatchMaking.py
@@ -3,4 +3,8 @@ from itertools import combinations
 arr=[('bleh',85),('Aequitas',90),('Aks',87),('Iam',20),('bill',50)]
 m=2
 combi=combinations(arr,m)
-print(*combi)
+newarr=[]
+for i in combi:
+	avg=(i[0][1]+i[1][1])/m
+	newarr.append((i[0][0],i[1][0],avg))
+print(newarr)
