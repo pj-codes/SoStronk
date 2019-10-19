f0f306f083f4b411d654b600eb835b132fa20acd
Pulkit <jainpulkit206@gmail.com>
2019-10-20 02:57:12 +0530

    Made all valid combinations of 2vs2 matches sorted by quality

--- MatchMaking.py
+++ MatchMaking.py
@@ -8,7 +8,8 @@ for i in combi:
 	avg=(i[0][1]+i[1][1])/m
 	newarr.append((i[0][0],i[1][0],avg))
 
-finalteams=combinations(newarr,2)
-matches=sorted(finalteams,key=lambda x:abs(x[0][2]-x[1][2]))
+validmatches = list(match for match in combinations(newarr, 2) if (match[0][0] not in match[1] and match[0][1] not in match[1]))
+
+matches=sorted(validmatches,key=lambda x:abs(x[0][2]-x[1][2]))
 for i in matches:
 	print(i)
